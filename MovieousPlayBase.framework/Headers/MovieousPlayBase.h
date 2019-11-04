2858279a76e9fc4b5b93f7c352b5587117eb6c52
movieous <cloudop@movieous.video>
2019-11-04 16:29:42 +0800

    release v2.0.0

---
+++ MovieousPlayBase.framework/Headers/MovieousPlayBase.h
@@ -0,0 +1,19 @@
+//
+//  MovieousPlayBase.h
+//  MovieousPlayBase
+//
+//  Created by Chris Wang on 2019/10/24.
+//  Copyright © 2019 Movieous Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieousPlayBase.
+FOUNDATION_EXPORT double MovieousPlayBaseVersionNumber;
+
+//! Project version string for MovieousPlayBase.
+FOUNDATION_EXPORT const unsigned char MovieousPlayBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieousPlayBase/PublicHeader.h>
+
+#import <MovieousBase/MovieousBase.h>
---
+++ MovieousPlayBase.podspec
@@ -0,0 +1,11 @@
+Pod::Spec.new do |spec|
+  spec.ios.deployment_target = '8.0'
+  spec.name         = 'MovieousPlayBase'
+  spec.version      = `sh utils.sh get-version`
+  spec.homepage     = 'https://github.com/movieous-team/MovieousBase-Cocoa-Release'
+  spec.authors      = { 'movieous' => 'cloudop@movieous.video' }
+  spec.summary      = 'Movieous base library for iOS.'
+  spec.source       = { :git => 'https://github.com/movieous-team/MovieousBase-Cocoa-Release.git', :tag => "v#{spec.version}" }
+  spec.static_framework = true
+  spec.vendored_frameworks = 'MovieousPlayBase.framework'
+end
